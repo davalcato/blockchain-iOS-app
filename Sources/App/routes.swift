d3235ddfe830f753f80e7762da52e1b4db799e41
davalcato <davalcato@yahoo.com>
2020-01-27 18:44:55 -0800

    added router.get("blockchain", use :blockchainController.getBlockchain) to the router for the service to host.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,7 @@ public func routes(_ router: Router) throws {
     // Basic "It works" example
     let blockchainController = BlockchainController()
     router.get("hello", use: blockchainController.greet)
-//    router.get("blockchain", use :blockchainController.getBlockchain)
+    router.get("blockchain", use :blockchainController.getBlockchain)
 //    router.post(Transaction.self, at: "mine", use: blockchainController.mine)
 //    router.post([BlockchainNode].self, at: "/nodes/register", use: blockchainController.registerNodes)
 //    router.get("/nodes", use :blockchainController.getNodes)
