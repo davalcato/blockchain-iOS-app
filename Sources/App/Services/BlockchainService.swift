8294fcec88979e40d27c70b393347cbf30589ebc
davalcato <davalcato@yahoo.com>
2020-01-27 18:27:05 -0800

    created the blockchainService in the services folded

---
+++ Sources/App/Services/BlockchainService.swift
@@ -0,0 +1,47 @@
+//
+//  BlockchainService.swift
+//  App
+//
+//  Created by Daval Cato on 1/27/20.
+//
+
+import Foundation
+
+class BlockchainService {
+    
+//    private (set) var blockchain :Blockchain!
+//
+//    init() {
+//        self.blockchain = Blockchain(genesisBlock: Block())
+//
+//    }
+//
+//    func getNodes() -> [BlockchainNode] {
+//        return self.blockchain.nodes
+//
+//    }
+//
+//    func registerNodes(nodes :[BlockchainNode]) -> [BlockchainNode] {
+//        return self.blockchain.registerNodes(nodes: nodes)
+//
+//    }
+//
+//    func getNextBlock(transactions : [Transaction]) -> Block {
+//
+//        let block = self.blockchain.getNextBlock(transactions: transactions)
+//        self.blockchain.addBlock(block)
+//        return block
+//    }
+//
+//    func getBlockchain() -> Blockchain {
+//        return self.blockchain
+        
+        
+    }
+
+
+
+
+
+
+
